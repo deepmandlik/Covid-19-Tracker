05e03cb48b18dac755a4629768b888855ce8942f
deepmandlik <mandlikdeep20@gmail.com>
2020-07-24 17:27:11 +0530

    Updates

---
+++ precache-manifest.5a0b4fd2cb801422e6e2f40f95e8b4d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09c706cc9c5d3d03974143e90a9bfa93",
+    "url": "/Covid-19-Tracker/index.html"
+  },
+  {
+    "revision": "0174bc2649b37389096c",
+    "url": "/Covid-19-Tracker/static/css/main.f80c2a15.chunk.css"
+  },
+  {
+    "revision": "47aa4dda19d2adaaebe3",
+    "url": "/Covid-19-Tracker/static/js/2.903ca026.chunk.js"
+  },
+  {
+    "revision": "b4cabae32341920cfb4131dbdebda2c2",
+    "url": "/Covid-19-Tracker/static/js/2.903ca026.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0174bc2649b37389096c",
+    "url": "/Covid-19-Tracker/static/js/main.63d3c17d.chunk.js"
+  },
+  {
+    "revision": "703c5a6d388b41827333",
+    "url": "/Covid-19-Tracker/static/js/runtime-main.03e43dbd.js"
+  },
+  {
+    "revision": "0c768863b084475e3760cf618d154919",
+    "url": "/Covid-19-Tracker/static/media/github.0c768863.png"
+  },
+  {
+    "revision": "babb97cae8f744754dc0560533bf4fce",
+    "url": "/Covid-19-Tracker/static/media/home.babb97ca.png"
+  },
+  {
+    "revision": "5cc248b95b5b6e7c9ad7909dadc137f4",
+    "url": "/Covid-19-Tracker/static/media/photo.5cc248b9.jpg"
+  }
+]);
